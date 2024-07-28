2071301718fc741aa066e9f404a8e5788da4a221
SoubarnikaS <125237552+SoubarnikaS@users.noreply.github.com>
2024-07-28 17:07:15 +0530

    Update tailwind.config.js

--- dvaral/tailwind.config.js
+++ dvaral/tailwind.config.js
@@ -1,36 +1,10 @@
-// const withMT = require("@material-tailwind/react/utils/withMT");
-
-// module.exports = withMT({
-//   content: ["./index.html", "./src//*.{js,ts,jsx,tsx}"],
-//   theme: {
-//     extend: {
-//       fontFamily: {
-//         'dancing-script': ['"Dancing Script"', 'cursive'],
-//         'acme': ['"Acme"', 'sans-serif'],
-//         'poppins': ['"Poppins"', 'sans-serif'],
-//         'merienda': ['"Merienda"', 'cursive'],
-//       },
-//     },
-//   },
-//   plugins: [],
-// });
-
-/** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    './index.html',
+    './src//*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {
-      fontFamily: {
-                'dancing-script': ['"Dancing Script"', 'cursive'],
-                'acme': ['"Acme"', 'sans-serif'],
-                'poppins': ['"Poppins"', 'sans-serif'],
-                'merienda': ['"Merienda"', 'cursive'],
-                'josefin-sans': ['"Josefin Sans"', 'sans-serif'],
-              },
-    },
+    extend: {},
   },
   plugins: [],
 }
